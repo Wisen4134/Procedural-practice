3b77e814b42ee03b490670dc25c4297c7dd882b9
wisen <wisen.huang@acculite.com.tw>
2023-07-17 17:25:23 +0800

    *Version 0.0.2     1. 大整理     2. 新增【LinkedList】連結串列

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,16 +12,6 @@ namespace ConsoleApp2
 {
     internal class Program
     {
-        public class DataStreet
-        {
-            public int val; public DataStreet next;
-            public DataStreet(int val = 0, DataStreet next= null)
-            {
-                this.val = val;
-                this.next = next;
-            }
-        }
-
         private static Random random = new Random();
 
         /// <summary>
@@ -29,70 +21,33 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Thread.Sleep(1);
-            PrintBubbleSortPractice();
+            Algorithm.PrintBubbleSortPractice();
             Thread.Sleep(1);
-            PrintStackPractice();
+            DataStructure.PrintStackPractice();
             Thread.Sleep(1);
-            PrintQueuePractice();
+            DataStructure.PrintQueuePractice();
             Thread.Sleep(1);
             PrintGeneric();
 
             Console.Write($"費氏數列驗證，請輸入數字：");
-            Console.WriteLine("結果為：" + PrintFibonacci(Int32.Parse(Console.ReadLine())));
-
+            Console.WriteLine("結果為：" + Algorithm.PrintFibonacci(Int32.Parse(Console.ReadLine())));
 
             Console.ReadLine();
 
         }
-        private static void PrintBubbleSortPractice()
-        {
-            List<int> SourceList = new List<int>();
-            for (int i = 0; i < 20; i++)
-            {
-                SourceList.Add(random.Next(1,1000));
-            }
-            var Result = BubbleSortArray(SourceList);
-            foreach (int i in Result)
-            {
-                Console.WriteLine($"{i}\t");
-            }
-            Console.WriteLine("\r\n");
-        }
-
-        /// <summary>
-        /// 【演算法】氣泡排序法
-        /// </summary>
-        /// <param name="item"></param>
-        /// <returns></returns>
-        private static List<int> BubbleSortArray(List<int> item)
-        {
-            var temp = 0;
-            for (int i = 0; i < item.Count - 1; i++)
-            {
-                for (int j = 0; j < item.Count - i - 1; j++)
-                {
-                    if (item[j] > item[j + 1])
-                    {
-                        temp = item[j];
-                        item[j] = item[j + 1];
-                        item[j + 1] = temp;
-                    }
-                }
-            }
-
-
-            return item;
-        }
+        
         private static void PrintGeneric()
         {
             object obj = new object();
-            obj = new DataStreet(0);
+            obj = new Card();
             Console.WriteLine($"來看看int ：{PrintSomeType<int>(1)}");
             Console.WriteLine($"來看看string ：{PrintSomeType<string>("字串啦")}");
             Console.WriteLine($"來看看double ：{PrintSomeType<double>(1.5)}");
             Console.WriteLine($"來看看object ：{PrintSomeType<object>(obj)}");
             
         }
+
+
 
         /// <summary>
         /// 【程式設計概念】泛型
@@ -104,66 +59,128 @@ namespace ConsoleApp2
         {
             return $"泛型應用：{para} 類型為：{typeof(EnterTypeHere)}";
         }
-
-        /// <summary>
-        /// 【資料結構】堆疊 FILO
-        /// </summary>
-        private static void PrintStackPractice() 
+        public class DataStructure
         {
-            Stack<int> Stack = new Stack<int>();
-            for (int i = 0; i < 10; i++)
+            /// <summary>
+            /// 【資料結構】連結串列
+            /// </summary>
+            public class LinkedList
             {
-                var number = random.Next(1, 100);
-                Stack.Push(number);
-                Console.WriteLine($"Stack 放入:{number}");
+                public class ListNode
+                {
+                    public int val;
+                    public ListNode next;
+                    public ListNode(int val = 0 , ListNode next = null)
+                    {
+                        this.val = val;
+                        this.next = next;
+                    }
+                }
             }
-            int n = Stack.Count;
-            for (int i = 0; i < n; i++)
+            /// <summary>
+            /// 【資料結構】佇列 FIFO
+            /// </summary>
+            public static void PrintQueuePractice()
             {
+                Queue<int> queue = new Queue<int>();
+                for (int i = 0; i < 10; i++)
+                {
+                    var number = random.Next(1, 100);
+                    queue.Enqueue(number);
+                    Console.WriteLine($"Queue 放入:{number}");
+                }
+                int qn = queue.Count;
+                for (int i = 0; i < qn; i++)
+                {
+                    Console.WriteLine($"Queue 拿出來：{queue.Dequeue()}");
+                }
+                Console.WriteLine("\r\n");
+            }
 
-                Console.WriteLine($"Stack 拿出來：{Stack.Pop()}");
+            /// <summary>
+            /// 【資料結構】堆疊 FILO
+            /// </summary>
+            public static void PrintStackPractice()
+            {
+                Stack<int> Stack = new Stack<int>();
+                for (int i = 0; i < 10; i++)
+                {
+                    var number = random.Next(1, 100);
+                    Stack.Push(number);
+                    Console.WriteLine($"Stack 放入:{number}");
+                }
+                int n = Stack.Count;
+                for (int i = 0; i < n; i++)
+                {
+
+                    Console.WriteLine($"Stack 拿出來：{Stack.Pop()}");
+                }
+                Console.WriteLine("\r\n");
             }
-            Console.WriteLine("\r\n");
         }
 
-        /// <summary>
-        /// 【資料結構】佇列 FIFO
-        /// </summary>
-        private static void PrintQueuePractice()
+        public class Algorithm
         {
-            Queue<int> queue = new Queue<int>();
-            for (int i = 0; i < 10; i++)
+
+            /// <summary>
+            /// 【演算法】費氏數列(斐波那契數列)
+            /// </summary>
+            /// <param name="n"></param>
+            /// <returns></returns>
+            public static int PrintFibonacci(int n)
             {
-                var number = random.Next(1, 100);
-                queue.Enqueue(number);
-                Console.WriteLine($"Queue 放入:{number}");
+                if (n < 1)
+                    return 0;
+                else if (n == 1)
+                    return 1;
+                else
+                {
+                    return PrintFibonacci(n - 1) + PrintFibonacci(n - 2);
+                }
+
             }
-            int qn = queue.Count;
-            for (int i = 0; i < qn; i++)
+
+            public static void PrintBubbleSortPractice()
             {
-                Console.WriteLine($"Queue 拿出來：{queue.Dequeue()}");
+                List<int> SourceList = new List<int>();
+                for (int i = 0; i < 20; i++)
+                {
+                    SourceList.Add(random.Next(1, 1000));
+                }
+                var Result = BubbleSortArray(SourceList);
+                foreach (int i in Result)
+                {
+                    Console.WriteLine($"{i}\t");
+                }
+                Console.WriteLine("\r\n");
             }
-            Console.WriteLine("\r\n");
+
+            /// <summary>
+            /// 【演算法】氣泡排序法
+            /// </summary>
+            /// <param name="item"></param>
+            /// <returns></returns>
+            private static List<int> BubbleSortArray(List<int> item)
+            {
+                var temp = 0;
+                for (int i = 0; i < item.Count - 1; i++)
+                {
+                    for (int j = 0; j < item.Count - i - 1; j++)
+                    {
+                        if (item[j] > item[j + 1])
+                        {
+                            temp = item[j];
+                            item[j] = item[j + 1];
+                            item[j + 1] = temp;
+                        }
+                    }
+                }
+
+
+                return item;
+            }
         }
 
-        /// <summary>
-        /// 【演算法】費氏數列(斐波那契數列)
-        /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        private static int PrintFibonacci(int n)
-        {
-            if (n < 1)
-                return 0;
-            else if (n == 1)
-                return 1;
-            else
-            {
-                return PrintFibonacci(n - 1) + PrintFibonacci(n - 2);
-            }
-            
-        }
-        
         public class Card
         {
             private int _id;
@@ -184,12 +201,25 @@ namespace ConsoleApp2
                 get { return this._content; }
                 set { this._content = value; }
             }
-            private string GetName()
+
+            public Card()
             {
-                return this.Name;
+                this._id = 0;
+                this._name = string.Empty;
+                this._content = string.Empty;
+            }
+
+
+        }
+
+        public class Dual : Card
+        {
+            private void Attack()
+            {
+                var s = this.Name;
             }
         }
 
-        
+
     }
 }
